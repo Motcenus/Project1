c81210ad0de961d8981923a560098f6350862194
Mohcine Otmane <84780158+Motcenus@users.noreply.github.com>
2024-04-21 16:37:47 +0100

    ..

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let btn = document.getElementById("btn");
 let nameinput = document.getElementById("name");
 let ageinput = document.getElementById("age");
 let cityeinput = document.getElementById("city");
-
+let i=0;
 
 let people = [];
 
+let peoplesize = people.length;
 
 btn.onclick = function() {
-    
     if(nameinput.value !="") {
         let person = {
             name:nameinput.value,
@@ -25,22 +25,24 @@ btn.onclick = function() {
         alert("Enter a person");
     }
 
-    for(let i=0;i<people.length;i++){
-        let card = document.createElement("div");
-        card.classList.add("cardstyle");
-        let namea = document.createElement("div");
-        namea.innerText = "Name: "+ people[i].name;
-        card.appendChild(namea);
-
-        let agea = document.createElement("div");
-        agea.innerText = "Age: "+ people[i].age;
-        card.appendChild(agea);
-
-        let citya = document.createElement("div");
-        citya.innerText = "City: "+ people[i].city;
-        card.appendChild(citya);
-        document.body.appendChild(card);
+    
+    if (peoplesize!=people.length){
+        for(i;i<people.length;i++){
+            let card = document.createElement("div");
+            card.classList.add("cardstyle");
+            let namea = document.createElement("div");
+            namea.innerText = "Name: "+ people[i].name;
+            card.appendChild(namea);
+        
+            let agea = document.createElement("div");
+            agea.innerText = "Age: "+ people[i].age;
+            card.appendChild(agea);
+        
+            let citya = document.createElement("div");
+            citya.innerText = "City: "+ people[i].city;
+            card.appendChild(citya);
+            document.body.appendChild(card);
+        }
     }
 }
-
 
